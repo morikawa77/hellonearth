561ae8a15f571eaaa8ed5296896857dfeda5a62a
Julio <leiramoura@live.com>
2019-09-20 18:31:47 -0300

    Tentando arrumar anjo

--- Assets/Scripts/EnemyAngel.cs
+++ Assets/Scripts/EnemyAngel.cs
@@ -6,39 +6,39 @@ public class EnemyAngel : MonoBehaviour
 {
     public GritoMorte gritoMorte;
     Animator animator;
+    bool grito = false;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
     }
-
+  
     // Update is called once per frame
     void Update()
     {
         
     }
-    public void Animacao()
+
+    public void AtaqueGrito()
     {
+        grito = true;
         animator.Play("Anjo-Attack");
+        StartCoroutine(Tempo());
     }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
 
-
-            Animacao();
-            StartCoroutine(Tempo());
-            
-
-
-        }
-    }
     public IEnumerator Tempo()
     {
         yield return new WaitForSeconds(6f);
-        Destroy(gameObject);
+        Ataque();
 
     }
-    
+    //public EnemyAngel inimigo;
+    public void Ataque()
+    {
+
+        animator.Play("Anjo-Idle");
+        Destroy(gameObject);
+    }
+
+
 }
--- Assets/Scripts/GritoMorte.cs
+++ Assets/Scripts/GritoMorte.cs
@@ -4,31 +4,23 @@ using UnityEngine;
 
 public class GritoMorte : MonoBehaviour
 {
-
-    public EnemyAngel inimigo;
-    
+    EnemyAngel anjo;
     Animator animator;
-    
+    bool grito = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        animator = GetComponent<Animator>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
-            collision.GetComponent<EnemyAngel>().Animacao();
-            Destroy(gameObject);
+
+
+
+            collision.GetComponent<EnemyAngel>().AtaqueGrito();
+
+
 
         }
     }
-   
+    
+
 }
