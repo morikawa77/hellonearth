0c7b1590088589e5a2a4eccfb01dd7ef17ed02c9
Reginaldo Morikawa <morikawa77@gmail.com>
2019-09-24 22:14:01 -0300

    Added audio on angel attack

--- Assets/Scripts/EnemyAngel.cs
+++ Assets/Scripts/EnemyAngel.cs
@@ -2,46 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class EnemyAngel : MonoBehaviour
 {
-    Animator animator;
-    bool grito = false;
-    // Start is called before the first frame update
-    void Start()
+  Animator animator;
+  bool grito = false;
+
+  AudioSource audioData;
+  void Start()
+  {
+    animator = GetComponent<Animator>();
+    audioData = GetComponent<AudioSource>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+  private void OnTriggerEnter2D(Collider2D collision)
+  {
+    if (collision.gameObject.CompareTag("Player"))
     {
-        animator = GetComponent<Animator>();
+      grito = true;
+      animator.SetBool("grito", grito);
+      audioData.Play(0);
+      StartCoroutine(Tempo());
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            grito = true;
-            animator.SetBool("grito", grito);
-            StartCoroutine(Tempo());
-
-        }
-    }
+  }
 
 
-    public IEnumerator Tempo()
-    {
-        yield return new WaitForSeconds(6f);
-        Ataque();
+  public IEnumerator Tempo()
+  {
+    yield return new WaitForSeconds(6f);
+    Ataque();
 
-    }
-    //public EnemyAngel inimigo;
-    public void Ataque()
-    {
+  }
+  //public EnemyAngel inimigo;
+  public void Ataque()
+  {
+    audioData.Stop();
+    animator.Play("Anjo-Idle");
 
-        animator.Play("Anjo-Idle");
-
-    }
+  }
 
 
 }
